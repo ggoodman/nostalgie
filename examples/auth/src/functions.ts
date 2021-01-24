d098e7b2e4ad87cd5680bd1a3c606212275774fc
Geoff Goodman <ggoodman@gmail.com>
2021-01-23 22:56:07 -0500

    Add authentication example

---
+++ examples/auth/src/functions.ts
@@ -0,0 +1,7 @@
+import { ServerFunctionContext } from 'nostalgie/functions';
+
+export function hello(ctx: ServerFunctionContext) {
+  return `Hello ${
+    ctx.auth.isAuthenticated ? (ctx.auth.credentials.user as any).name : '<Anonymous>'
+  }`;
+}
