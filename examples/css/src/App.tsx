e4609e5af236ac7dab973b77e4462b79f00a80b2
Geoff Goodman <ggoodman@gmail.com>
2021-01-27 09:27:32 -0500

    Minor polish on css example

--- examples/css/src/App.tsx
+++ examples/css/src/App.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'nostalgie/helmet';
 import { Link, Route, Switch } from 'nostalgie/routing';
 import * as React from 'react';
 import './red.css';
@@ -6,21 +7,41 @@ import './shared.css';
 export default function () {
   return (
     <>
-      <h1>Hello Red</h1>
-      <p>
-        {`The `}
-        <code>h1</code>
-        {` tag above should be red. Additionally, the imported css should have been included in the initial render.`}
-      </p>
-      <hr />
-      <React.Suspense fallback="Loading...">
-        <Switch>
-          <Route path="/blue" component={React.lazy(() => import('./Blue'))}></Route>
-          <Route path="/" exact>
-            <Link to="/blue">Blue</Link>
-          </Route>
-        </Switch>
-      </React.Suspense>
+      <Helmet>
+        <title>CSS loading and code-splitting example · Nostalgie</title>
+        <meta
+          name="description"
+          content="A simple example of using Nostalgie to load .css files such that they're correctly split between page loads while being included in the initial render if needed."
+        />
+      </Helmet>
+      <main className="prose container mx-auto">
+        <h1>Hello Red</h1>
+        <p>
+          {`The `}
+          <code>h1</code>
+          {` tag above should be red. Additionally, the imported css should have been included in the initial render.`}
+        </p>
+        <p>
+          {`The `}
+          <code>h1</code>
+          {` tag above should also be underlined. The underline rule comes from the `}
+          <code>shared.css</code>
+          {` file. That file is `}
+          <em>also</em>
+          {` a dependency of the `}
+          <code>/blue</code>
+          {` route. This example demonstrates that the shared css will correctly be included in the initial render for both routes while never being loaded more than once.`}
+        </p>
+        <hr />
+        <React.Suspense fallback="Loading...">
+          <Switch>
+            <Route path="/blue" component={React.lazy(() => import('./Blue'))}></Route>
+            <Route path="/" exact>
+              <Link to="/blue">Blue</Link>
+            </Route>
+          </Switch>
+        </React.Suspense>
+      </main>
     </>
   );
 }
