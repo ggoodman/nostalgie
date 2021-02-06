abebb5b00e33449771f65502ca5b8a73eb9d45d8
Geoff Goodman <ggoodman@gmail.com>
2021-02-05 21:08:03 -0500

    Introduce template package

---
+++ packages/template/src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="flex flex-row items-center justify-center">
+      <main className="px-12 py-16 text-center">
+        <h1 className="text-6xl font-semibold text-gray-900 leading-none">Hello Nostalgie</h1>
+        <h2 className="mt-6 text-xl font-light text-true-gray-500 antialiased">Goodbye Friction</h2>
+      </main>
+    </div>
+  );
+}
