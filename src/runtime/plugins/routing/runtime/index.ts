7d2d5907bb52d5f5af4d1110c1d4a41ba57dd9d2
Geoff Goodman <ggoodman@gmail.com>
2022-04-20 13:46:18 -0400

    routing: RouteBoundary concept

--- src/runtime/plugins/routing/runtime/index.ts
+++ src/runtime/plugins/routing/runtime/index.ts
@@ -1,20 +1,41 @@
-import type { ReactElement } from 'react';
-import * as React from 'react';
-import type { LoadState } from '../../lazy/runtime/state';
+import type { ComponentType } from 'react';
+import {
+  createContext,
+  createElement,
+  ReactElement,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import type { LoadState, Resolved } from '../../lazy/runtime/state';
 
 export { Link } from 'react-router-dom';
+
+export interface RouteLoadState {
+  status: LoadState['status'];
+  setStatus: (status: LoadState['status']) => void;
+}
+
+const RouteBoundaryContext = createContext<RouteLoadState | null>(null);
 
 export function createRoute(useLazy: () => LoadState): ReactElement {
   function LazyRoute(): ReactElement {
     const loadState = useLazy();
+    const boundary = useContext(RouteBoundaryContext);
+
+    useEffect(() => {
+      if (boundary) {
+        boundary.setStatus(loadState.status);
+      }
+    });
 
     switch (loadState.status) {
       case 'error':
         throw loadState.error;
       case 'idle':
-        return React.createElement('div', null, 'Idle');
+        return createElement('div', null, 'Idle');
       case 'loading':
-        return React.createElement('div', null, 'Loading...');
+        return createElement('div', null, 'Loading...');
       case 'success':
         if (import.meta.env.DEV) {
           if (loadState.component == null) {
@@ -23,9 +44,28 @@ export function createRoute(useLazy: () => LoadState): ReactElement {
             );
           }
         }
-        return React.createElement(loadState.component);
+
+        return createElement(RouteBoundary, { loadState });
     }
   }
 
-  return React.createElement(LazyRoute);
+  return createElement(LazyRoute);
+}
+
+function RouteBoundary(props: { loadState: Resolved<ComponentType<any>> }) {
+  const [status, setStatus] = useState<LoadState['status']>(
+    () => props.loadState.status
+  );
+
+  return createElement(
+    RouteBoundaryContext.Provider,
+    {
+      value: {
+        status,
+        setStatus,
+      },
+    },
+    null,
+    createElement(props.loadState.component)
+  );
 }
